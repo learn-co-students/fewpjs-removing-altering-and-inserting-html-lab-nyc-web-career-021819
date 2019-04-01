30377e39f18c2bec439221da8b7a0aa81b3e8751
Kate Kim <katekim5809@gmail.com>
2019-04-01 18:21:26 -0400

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ document.getElementById('main').remove()
 let newHeader= document.createElement('h1')
 newHeader.id = "victory"
 newHeader.innerText = "YOUR-NAME is the champion"
-document.body.appendChlid(newHeader)
