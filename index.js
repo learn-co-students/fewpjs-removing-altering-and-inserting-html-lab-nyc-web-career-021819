cc2be9005a36ffe5a9116dc3b29609aff43f408e
NickyEXE <dovern42@gmail.com>
2019-04-01 18:20:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+// newHeader.innerHTML = "YOUR-NAME is the champion"
+
+// let words = document.getElementById('h1#victory');
+
+document.body.appendChild(newHeader)
+newHeader.innerText = "YOUR-NAME is the champion"
