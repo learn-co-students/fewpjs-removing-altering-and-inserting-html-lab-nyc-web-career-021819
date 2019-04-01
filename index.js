200c60a24606b4043f77ee089656bc930d96fd35
Kate Kim <katekim5809@gmail.com>
2019-04-01 18:19:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove()
+let newHeader= document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerText = "YOUR-NAME is the champion"
+document.body.appendChlid(newHeader)
