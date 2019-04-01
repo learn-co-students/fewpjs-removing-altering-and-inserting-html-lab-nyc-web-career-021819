64b46550fa9c365d675ed426f0ac1d8179c726de
rrosztoczy <rrosztoczy@gmail.com>
2019-04-01 16:58:05 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.querySelector('main#main').remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerText = "RYAN ROSZTOCZY is the champion"
+document.body.appendChild(newHeader)
