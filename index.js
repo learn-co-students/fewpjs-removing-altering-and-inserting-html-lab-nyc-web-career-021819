cc15f4b161f27d36efba8463a6c297017e968cb4
richietam <richardtam.ny@gmail.com>
2019-04-03 21:22:44 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.querySelector("#main")
+
+main.remove()
+
+
+const newh1 = document.createElement('h1')
+newh1.setAttribute("id", "victory")
+document.body.appendChild(newh1)
+const newHeader = document.querySelector('h1#victory')
+newHeader.innerText = "YOUR-NAME is the champion"
